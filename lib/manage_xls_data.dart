50fd5dfb603e02e126cbab07c0c0f9a479db20d5
PierrickMonchoix <pierrick.monchoix@gmail.com>
2021-10-20 19:08:14 +0200

    Lecture d'un document exel ; tests OK

--- lib/manage_xls_data.dart
+++ lib/manage_xls_data.dart
@@ -1,7 +1,32 @@
-class ManageXlsData {
-  
-  static int getPrimaryData(){
-    return 11;
-  }
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:excel/excel.dart';
+import 'package:flutter/services.dart' show ByteData, rootBundle;
 
+class ManageXlsData {
+  static Future<int> getPrimaryData() async {
+
+    /* Your blah blah code here */
+
+    ByteData data = await rootBundle.load("assets/for_tests/for_flutter.xlsx"); 
+    var bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+    var excel = Excel.decodeBytes(bytes);
+
+/*
+    print(excel["Feuil1"].cell(CellIndex.indexByString("A1")).value);
+    for (var table in excel.tables.keys) {
+      print(table); //sheet Name
+      print(excel.tables[table]!.maxCols);
+      print(excel.tables[table]!.maxRows);
+      for (var row in excel.tables[table]!.rows) {
+        print("$row");
+      }
+    }
+*/
+    
+
+    int resultat = excel["Feuil1"].cell(CellIndex.indexByString("A1")).value;
+
+    return resultat;
+  }
 }
--- test/manage_xls_data_test.dart
+++ test/manage_xls_data_test.dart
@@ -1,12 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:first_flutter_app/manage_xls_data.dart';
 
+
 void main() {
   testWidgets('manage xls data ...', (tester) async {
     int result = 0;
 
-    result = ManageXlsData.getPrimaryData();
+    result = await ManageXlsData.getPrimaryData();
 
-    expect(result , 12);
+    expect(result , 13);
   });
 }

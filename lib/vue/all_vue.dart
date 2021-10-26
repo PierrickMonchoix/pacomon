49241dd2a8a95dc6fcb81f83e2be5a7479a43fba
PierrickMonchoix <pierrick.monchoix@gmail.com>
2021-10-26 16:41:18 +0200

    ajout des io jolis

--- lib/vue/all_vue.dart
+++ lib/vue/all_vue.dart
@@ -43,110 +43,138 @@ class _AllVueState extends State<AllVue> {
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           widget._vueScreen,
-          Expanded( // pesner a utiliser les Flexible
+          Expanded(
+            // pesner a utiliser les Flexible
             child: Container(
-                color: Colors.grey[800],
-                constraints: BoxConstraints.expand(),
-                /* 
+              color: Colors.grey[800],
+              constraints: BoxConstraints.expand(),
+              /* 
                 decoration: BoxDecoration(
                   border: Border.all(color: Colors.purple)
                 ), */
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    AspectRatio(
-                        aspectRatio: 7.0 / 3.0,
-                        child: Container(
-                          color: Colors.grey[800],
-                          constraints: BoxConstraints.expand(),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Row(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        AspectRatio(
+                            aspectRatio: 7.0 / 3.0,
+                            child: Container(
+                              constraints: BoxConstraints.expand(),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                mainAxisAlignment: MainAxisAlignment.start,
                                 children: [
-                                  Expanded(child: Container()),
-                                  Expanded(
-                                    child: AspectRatio(aspectRatio: 1,
-                                      child: BoutonLettre(
-                                        text: '↑',
-                                        function: {print("btn a")},
+                                  Row(
+                                    children: [
+                                      Expanded(child: Container()),
+                                      Expanded(
+                                        child: AspectRatio(
+                                          aspectRatio: 1,
+                                          child: BoutonLettre(
+                                            text: '↑',
+                                            function: {print("btn a")},
+                                          ),
+                                        ),
                                       ),
-                                    ),
+                                      Expanded(child: Container()),
+                                      Expanded(child: Container()),
+                                      Expanded(child: Container()),
+                                      Expanded(
+                                          child: AspectRatio(
+                                              aspectRatio: 1,
+                                              child: BoutonLettre(
+                                                text: 'x',
+                                                function: {print("btn a")},
+                                              ))),
+                                      Expanded(child: Container())
+                                    ],
                                   ),
-                                  Expanded(child: Container()),
-                                  Expanded(child: Container()),
-                                  Expanded(child: Container()),
-                                  Expanded(
-                                    child: AspectRatio(aspectRatio: 1,
-                                      child: BoutonLettre(
-                                    text: 'x',
-                                    function: {print("btn a")},
-                                  ))),
-                                  Expanded(child: Container())
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                          child: AspectRatio(
+                                        aspectRatio: 1,
+                                        child: BoutonLettre(
+                                          text: '←',
+                                          function: {print("btn a")},
+                                        ),
+                                      )),
+                                      Expanded(child: Container()),
+                                      Expanded(
+                                          child: AspectRatio(
+                                              aspectRatio: 1,
+                                              child: BoutonLettre(
+                                                text: '→',
+                                                function: {print("btn a")},
+                                              ))),
+                                      Expanded(child: Container()),
+                                      Expanded(
+                                          child: AspectRatio(
+                                              aspectRatio: 1,
+                                              child: BoutonLettre(
+                                                text: 'y',
+                                                function: {print("btn a")},
+                                              ))),
+                                      Expanded(child: Container()),
+                                      Expanded(
+                                          child: AspectRatio(
+                                              aspectRatio: 1,
+                                              child: BoutonLettre(
+                                                text: 'b',
+                                                function: {print("btn a")},
+                                              ))),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Expanded(child: Container()),
+                                      Expanded(
+                                        child: AspectRatio(
+                                          aspectRatio: 1,
+                                          child: BoutonLettre(
+                                            text: '↑',
+                                            function: {print("btn a")},
+                                          ),
+                                        ),
+                                      ),
+                                      Expanded(child: Container()),
+                                      Expanded(child: Container()),
+                                      Expanded(child: Container()),
+                                      Expanded(
+                                          child: AspectRatio(
+                                              aspectRatio: 1,
+                                              child: BoutonLettre(
+                                                text: 'x',
+                                                function: {print("btn a")},
+                                              ))),
+                                      Expanded(child: Container())
+                                    ],
+                                  )
                                 ],
                               ),
-                              Row(
-                                children: [
-                                  Expanded(
-                                    child: AspectRatio(aspectRatio: 1,
-                                    child: BoutonLettre(
-                                      text: '←',
-                                      function: {print("btn a")},
-                                    ),
-                                  )),
-                                  Expanded(child: Container()),
-                                  Expanded(child: AspectRatio(aspectRatio: 1,
-                                      child: BoutonLettre(
-                                    text: '→',
-                                    function: {print("btn a")},
-                                  ))),
-                                  Expanded(child: Container()),
-                                  Expanded(
-                                    child: AspectRatio(aspectRatio: 1,
-                                      child: BoutonLettre(
-                                    text: 'y',
-                                    function: {print("btn a")},
-                                  ))),
-                                  Expanded(child: Container()),
-                                  Expanded(
-                                    child: AspectRatio(aspectRatio: 1,
-                                      child: BoutonLettre(
-                                    text: 'b',
-                                    function: {print("btn a")},
-                                  ))),
-                                ],
-                              ),Row(
-                                children: [
-                                  Expanded(child: Container()),
-                                  Expanded(
-                                    child: AspectRatio(aspectRatio: 1,
-                                      child: BoutonLettre(
-                                        text: '↑',
-                                        function: {print("btn a")},
-                                      ),
-                                    ),
-                                  ),
-                                  Expanded(child: Container()),
-                                  Expanded(child: Container()),
-                                  Expanded(child: Container()),
-                                  Expanded(
-                                    child: AspectRatio(aspectRatio: 1,
-                                      child: BoutonLettre(
-                                    text: 'x',
-                                    function: {print("btn a")},
-                                  ))),
-                                  Expanded(child: Container())
-                                ],
-                              )
-                            ],
-                          ),
-                        ))
-                  ],
-                )
+                            ))
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(),
+                  ),
+                  Container(
+                    width: 1000,
+                    height: 20,
+                    child: Center(
+                      child: Text('C H E E S E B U R G E R  C O R P .',
+                          style: TextStyle(color: Colors.white)),
+                    ),
+                  )
+                ],
+              ),
 
-                /* Padding(
+              /* Padding(
                     padding: const EdgeInsets.all(10),
                     child : Center(child: 
                          Row(
@@ -181,7 +209,7 @@ class _AllVueState extends State<AllVue> {
                         ), 
                     )
                       ) */
-                ),
+            ),
 
             /* Column(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -218,7 +246,7 @@ class _AllVueState extends State<AllVue> {
                             style: TextStyle(color: Colors.white))
                       ],
                     ), */
-          )
+          ),
           /* child: Row(
                   children: [
                     Container(
